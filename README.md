cb41992bfc2ffbb05caa9b225034cc720fe2c80c
KingOfTheGamesYT <teamolympuscaptain@gmail.com>
2021-03-26 10:59:12 +1100

    Set up workspace
    first comit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Mystics
 Brings the universe of TVD/Originals/Legacies to Minecraft
+
+License is ARR
---
+++ src/main/java/com/devmaster/mystics/Mystics.java
@@ -0,0 +1,42 @@
+package com.devmaster.mystics;
+
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+@Mod("mystics")
+public class Mystics
+{
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public Mystics() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+
+    }
+
+    private void doClientStuff(final FMLClientSetupEvent event) {
+
+    }
+
+
+    @SubscribeEvent
+    public void onServerStarting(FMLServerStartingEvent event) {
+
+    }
+
+}
